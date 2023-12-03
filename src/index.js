1da419bdc9135b12f00fc7c29ddc9ef24429d7e6
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-03 13:30:28 +0000

    Added menu import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import { loadHomePage } from "./components/home";
+import { createMenu } from "./components/menu";
 import logo from './assets/images/fat_cat_logo.png';
 import './styles/style.css';
 
 
 document.getElementById('logo').src = logo;
 
-import { loadHomePage } from "./components/home";
 
 loadHomePage();
+createMenu();
