3ae00557a4c971b485af6c1bbcd73b6db97d690c
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-03 13:30:14 +0000

    Added basic starting code.

---
+++ src/components/menu.js
@@ -0,0 +1,11 @@
+
+export const createMenu = () => {
+    const content = document.getElementById('content');
+
+    const menu = document.createElement('div');
+    menu.classList.add = 'page-container';
+
+
+
+    content.appendChild(menu)
+}
