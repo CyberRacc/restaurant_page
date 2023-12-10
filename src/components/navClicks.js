ad68a20351d02685a80528801e21dc368676e1f9
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-10 23:14:14 +0000

    Implemented first draft of page switching logic

--- src/components/navClicks.js
+++ src/components/navClicks.js
@@ -1,14 +1,36 @@
+import { createMenu } from "./menu";
+import { loadHomePage } from "./home";
+
 const content = document.getElementById('content');
 
 export const enableNavClicks = () => {
     const navButtons = document.querySelectorAll('.nav-element');
 
+    // Below code is broken.
+    // Only the Menu button is working.
+
     navButtons.forEach( (button) => {
         button.addEventListener('click', (e) => {
             console.log(e);
             // Call page update to the relevant page.
+            if (e.id = "menu") {
+                content.innerHTML = '';
+                console.log("Loading Menu...");
+                createMenu();
+                return;
+            } else if (e.id = "bookings") {
+                console.log("Bookings not yet implemented.");
+                // createBookings()
+                return;
+            } else if (e.id = "home") {
+                console.log("Creating Home...");
+                loadHomePage();
+                return;
+            } else {
+                console.log("Invalid Selection");
+                return;
+            }
         });
     });
-
 }
 
