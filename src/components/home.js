12b35cd1a96f2117e812b41822b8cef695300f2a
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-12 23:17:47 +0000

    cleaned, converted to class, used default export

--- src/components/home.js
+++ src/components/home.js
@@ -1,32 +1,79 @@
-export const loadHomePage = () => {
+export default class Home {
     
-    console.log("Loading home page...");
+    // This method creates the home page content
+    createHomePage() {
+        console.log("Loading home page...");
 
-    const content = document.getElementById(`content`);
+        // Get the content element
+        const content = document.getElementById(`content`);
 
-    const heading = document.createElement('h1');
-    heading.textContent = 'Fat Cat Cafe'
+        // Create the page container
+        const homeContainer = document.createElement('div');
+        homeContainer.id = 'home-container';
 
-    const headingLogo = document.createElement('img');
-    headingLogo.src = './fat_cat_logo.png';
-    headingLogo.alt = 'logo of a large round cat with text saying fat cat';
+        // Create the heading container
+        const headingContainer = document.createElement('div');
+        headingContainer.classList.add('heading-container');
 
-    const tagline = document.createElement('p');
-    tagline.textContent = 'Welcome to Fat Cat! Wanna stuff your face with great food? Visit one of our locations.'
+        // Create the heading logo
+        const headingLogo = document.createElement('img');
+        headingLogo.src = '/src/assets/images/fat_cat_logo.png';
+        headingLogo.alt = 'logo of a large round cat with text saying fat cat';
+        headingLogo.classList.add('heading-logo');
 
-    const btnViewLocations = document.createElement('button');
-    btnViewLocations.textContent = 'View Locations';
-    btnViewLocations.id = 'btn-view-locations'
+        // Create the heading
+        const heading = document.createElement('h1');
+        heading.textContent = 'Fat Cat Cafe';
+        heading.classList.add('heading');
 
-    const btnViewMenu = document.createElement('button');
-    btnViewMenu.textContent = 'View menu';
-    btnViewMenu.id = 'btn-view-locations'
+        // Create the tagline
+        const tagline = document.createElement('p');
+        tagline.textContent = 'Welcome to Fat Cat Cafe! Indulge in our delicious food at any of our convenient locations.';
+        tagline.classList.add('tagline');
 
-    content.appendChild(heading);
-    content.appendChild(headingLogo);
-    content.appendChild(tagline);
-    content.appendChild(btnViewLocations);
-    content.appendChild(btnViewMenu);
+        // Create the "View Locations" button
+        const btnViewLocations = document.createElement('button');
+        btnViewLocations.textContent = 'View Locations';
+        btnViewLocations.id = 'btn-view-locations';
+        btnViewLocations.classList.add('btn-view-locations');
 
-    console.log("Home page loaded!")
-};
+        // Create the "View Menu" button
+        const btnViewMenu = document.createElement('button');
+        btnViewMenu.textContent = 'View menu';
+        btnViewMenu.id = 'btn-view-menu';
+        btnViewMenu.classList.add('btn-view-menu');
+
+        // Append elements to the heading container
+        headingContainer.appendChild(headingLogo);
+        headingContainer.appendChild(heading);
+
+        // Append elements to the content element
+        content.appendChild(headingContainer);
+        content.appendChild(tagline);
+        content.appendChild(btnViewLocations);
+        content.appendChild(btnViewMenu);
+        content.appendChild(homeContainer);
+
+        // Append elements to the heading container
+        headingContainer.appendChild(headingLogo);
+        headingContainer.appendChild(heading);
+        headingContainer.appendChild(tagline);
+
+        // Append elements to the page container
+        homeContainer.appendChild(headingContainer);
+        homeContainer.appendChild(btnViewLocations);
+        homeContainer.appendChild(btnViewMenu);
+        
+
+        console.log("Home page loaded!");
+    }
+
+    // This is a method within your class
+    loadHomePage() {
+        // When this method is called, it calls the createHomePage method on the same instance of the class
+        // 'this' refers to the instance of the class that the method is being called on
+        this.createHomePage();
+    }
+
+
+}
