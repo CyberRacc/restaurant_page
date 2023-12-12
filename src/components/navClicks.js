424e178f8ed0d788768801cadbb4a5105721155d
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-12 23:18:33 +0000

    added imports , converted if to switch

--- src/components/navClicks.js
+++ src/components/navClicks.js
@@ -1,33 +1,43 @@
-import { loadHomePage } from "./home";
+import Home from "./home";
+import { Bookings } from "./bookings";
 
 const content = document.getElementById('content');
+const homePage = new Home();
+const bookingsPage = new Bookings();
 
 export const enableNavClicks = () => {
     const navButtons = document.querySelectorAll('.nav-element');
-
-    // Below code is broken.
-    // Only the Menu button is working.
 
     navButtons.forEach( (button) => {
         button.addEventListener('click', (e) => {
             console.log(e);
             // Call page update to the relevant page.
-            if (e.id = "menu") {
-                content.innerHTML = '';
-                console.log("Loading Menu...");
-                // createMenu();
-                return;
-            } else if (e.id = "bookings") {
-                console.log("Bookings not yet implemented.");
-                // createBookings()
-                return;
-            } else if (e.id = "home") {
-                console.log("Creating Home...");
-                loadHomePage();
-                return;
-            } else {
-                console.log("Invalid Selection");
-                return;
+            switch (button.id) {
+                case "menu":
+                    // Clear the content
+                    content.innerHTML = '';
+                    console.log("Loading Menu...");
+                    // createMenu();
+                    // 'break' is used to exit the switch statement after a matching case is found.
+                    // 'return' is not necessary here because we're not trying to stop the execution of the entire function,
+                    // just the switch statement. If this was an 'if' statement and we wanted to stop the execution of the function
+                    // after a matching condition was found, we would use 'return'.
+                    break;
+                case "bookings":
+                    // Clear the content
+                    content.innerHTML = '';
+                    console.log("Loading Bookings...");
+                    bookingsPage.createBookings();
+                    break; // Same explanation as above
+                case "home":
+                    // Clear the content
+                    content.innerHTML = '';
+                    console.log("Creating Home...");
+                    homePage.loadHomePage();
+                    break; // Same explanation as above
+                default:
+                    // If button.id doesn't match any of the above cases, log "Invalid Selection"
+                    console.log("Invalid Selection");
             }
         });
     });
