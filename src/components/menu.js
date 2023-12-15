b835b9228d1113c5e57295399253451a5d8ed13c
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-15 00:27:43 +0000

    Cleaned up code, menu items now added to DOM

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,95 +1,81 @@
-const content = document.getElementById('content');
+import MenuItem from './cafeMenuItems.js';
 
-import './cafeMenuItems';
-
-class Menu {
+export default class Menu {
     constructor() {
         this.items = [];
     }
 
-    addItem(item) {
+    content = document.getElementById('content');
+
+    addItemToArray(item) {
         this.items.push(item);
-        this.updateMenu();
+    }
+
+    initialiseItems() {
+        this.items = []; // Clear the items array
+        this.addItemToArray(new MenuItem("Donut", "Donut is good!", 6.50, '/src/assets/images/fat_cat_donut.png'));
+        this.addItemToArray(new MenuItem("Ring Donut", "Cute ring donut", 6.00, '/src/assets/images/fat_cat_ring_donut.png'));
+        this.addItemToArray(new MenuItem("Cookie", "Cookie is nice!", 4.00, '/src/assets/images/fat_cat_cookie.png'));
+        this.addItemToArray(new MenuItem("Coffee", "Nice hot, cat coffee!", 8.00, '/src/assets/images/fat_cat_coffee.png'));
+        this.addItemToArray(new MenuItem("Cupcake", "Cake of cup", 6.50, '/src/assets/images/fat_cat_cupcake.png'));
+        this.addItemToArray(new MenuItem("Hot Chocolate", "Soothing hot chocolate with extra chocolate", 7.50, '/src/assets/images/fat_cat_hot_chocolate.png'));
+        this.addItemToArray(new MenuItem("Macaroon", "Lovely macaroon with cute cats!", 4.50, '/src/assets/images/fat_cat_macaroon.png'));
+        this.addItemToArray(new MenuItem("Pancakes", "Succulent, fresh pancakes to brighten your day!", 11.00, '/src/assets/images/fat_cat_pancakes.png'));
+        this.addItemToArray(new MenuItem("Pudding", "Soft, creamy custard pudding with rich double cream!", 10.50, '/src/assets/images/fat_cat_pudding.png'));
+        this.addItemToArray(new MenuItem("Burger", "Succulent beef-free burger with a cat bun! Comes with a drink.", 10.00, '/src/assets/images/fat_cat_burger.png'));
+        this.addItemToArray(new MenuItem("Christmas Hot Chocolate", "Rich, creamy hot chocolate with cinnamon!", 8.50, '/src/assets/images/fat_cat_christmas_hot_chocolate.png'));
     }
 
     updateMenu() {
+        console.log("Updating menu...");
         // Clear existing items
         const menu = document.getElementById('menu-grid');
-        menu.innerHTML = '';
+        console.table(this.items)
 
-        // Maybe a foreach loop here?
         // Create a menu box for each item in the items array above.
         this.items.forEach(item => {
+            // Create elements
             const menuItem = document.createElement('div');
-            menuItem.classList.add('menu-item');
-
+            const itemDescContainer = document.createElement('div');
+            const image = document.createElement('img');
             const title = document.createElement('h3');
-            title.textContent = item.title;
-            menuItem.appendChild(title);
-
             const desc = document.createElement('p');
-            desc.textContent = item.desc;
-            menuItem.appendChild(desc);
-
             const price = document.createElement('p');
-            price.textContent = item.price;
-            menuItem.appendChild(price);
 
+            // Add classes
+            menuItem.classList.add('menu-item');
+            itemDescContainer.classList.add('menu-item-desc');
+
+            // Append elements
+            menuItem.appendChild(image);
+            menuItem.appendChild(itemDescContainer);
+            itemDescContainer.appendChild(title);
+            itemDescContainer.appendChild(desc);
+            itemDescContainer.appendChild(price);
             menu.appendChild(menuItem);
+
+            // Set content
+            image.src = item.imgSrc;
+            title.textContent = item.title;
+            desc.textContent = item.desc;
+            price.textContent = item.price.toString();
         });
     }
 
     // Method for creating the menu container.
     createMenu = () => {
+        console.log("Creating menu...");
         // Create elements
         const menu = document.createElement('div');
         const menuGrid = document.createElement('div');
-        const menuItem = document.createElement('div');
-        const menuItemImageContainer = document.createElement('div');
-        const menuItemImage = document.createElement('img');
-        const menuItemDesc = document.createElement('div');
-        const menuItemDescTitle = document.createElement('h2');
-        const menuItemDescPara = document.createElement('p');
 
         // Add classes & IDs
         menuGrid.id = 'menu-grid';
 
         menu.classList.add('page-container');
-        menuItem.classList.add('menu-item');
-        menuItemImageContainer.classList.add('menu-item-image');
 
-        menuItemImage.src = '/src/assets/images/';
-
+        // Append elements
         content.appendChild(menu)
+        menu.appendChild(menuGrid);
     }
 }
-
-// Create Menu instance
-const menu = new Menu();
-menu.createMenu();
-
-// Create MenuItem instances
-let donut = new MenuItem("Donut", "Donut is good!", 6.50);
-let ringDonut = new MenuItem("Ring Donut", "Cute ring donut", 6.00);
-let cookie = new MenuItem("Cookie", "Cookie is nice!", 4.00);
-let coffee = new MenuItem("Coffee", "Nice hot, cat coffee!", 8.00);
-let cupcake = new MenuItem("Cupcake", "Cake of cup", 6.50);
-let hotChocolate = new MenuItem("Hot Chocolate", "Soothing hot chocolate with extra chocolate", 7.50);
-let macaroon = new MenuItem("Macaroon", "Lovely macaroon with cute cats!", 4.50);
-let pancakes = new MenuItem("Pancakes", "Succulent, fresh pancakes to brighten your day!", 11.00);
-let pudding = new MenuItem("Pudding", "Soft, creamy custard pudding with rich double cream!", 10.50);
-let burger = new MenuItem("Burger", "Succulent beef-free burger with a cat bun! Comes with a drink.", 10.00);
-let christmasHotChoc = new MenuItem("Christmas Hot Chocolate", "Rich, creamy hot chocolate with cinnamon!", 8.50);
-
-// Add items to the menu
-menu.addItem(donut);
-menu.addItem(ringDonut);
-menu.addItem(cookie);
-menu.addItem(coffee);
-menu.addItem(cupcake);
-menu.addItem(hotChocolate);
-menu.addItem(macaroon);
-menu.addItem(pancakes);
-menu.addItem(pudding);
-menu.addItem(burger);
-menu.addItem(christmasHotChoc);
