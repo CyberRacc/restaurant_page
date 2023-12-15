b4c013d3faa6bb51720e4cd1b084af2efa95c4ac
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-15 18:47:58 +0000

    Removed string concatenation that was causing NaN errors later

--- src/components/cafeMenuItems.js
+++ src/components/cafeMenuItems.js
@@ -2,7 +2,7 @@ export default class MenuItem {
     constructor(title, desc, price, imgSrc) {
         this.title = "Fat Cat" + " " + title;
         this.desc = desc;
-        this.price = "£" + price;
+        this.price = price;
         this.imgSrc = imgSrc;
     }
 }
