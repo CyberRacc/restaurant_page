ee14052d162f9118bb087e1b405c489d632a7d57
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-15 18:50:12 +0000

    constructor for creating new locations

---
+++ src/components/locationStorage.js
@@ -0,0 +1,10 @@
+export default class locationsList {
+    constructor(country, city, address, phone, email, hours) {
+        this.country = country;
+        this.city = city;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.hours = hours;
+    }
+}
