86dd5566afcf2bb3a6439216c9be46251f0b3a1e
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2023-12-17 23:10:30 +0000

    Added dark theme toggle through JavaScript and an SVG icon

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Home from "./components/home";
 import { enableNavClicks } from "./components/navClicks";
 import logo from './assets/images/fat_cat_logo_2_alpha.png';
 import darkThemeIcon from './assets/icons/dark_mode_moon.svg';
+import darkThemeIconFilled from './assets/icons/dark_mode_moon_filled.svg';
 
 // Import styles
 import './styles/global_style.css';
@@ -21,19 +22,38 @@ if (logoElement) {
     console.error('Logo element not found');
 }
 
-const iconElement = document.getElementById('theme-switch');
-if (iconElement) {
-    iconElement.src = darkThemeIcon;
+// Get the HTML element with the id 'theme-switch-container'. This should be a container element where the SVG will be inserted.
+const iconElement = document.getElementById('theme-switch-container');
+
+// Check if the user prefers dark mode.
+const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+// If the user prefers dark mode, add the 'dark-theme' class to the document element and set the SVG to the dark theme icon.
+if (prefersDarkMode) {
+    document.documentElement.classList.add('dark-theme');
+    iconElement.innerHTML = darkThemeIconFilled;
 } else {
-    console.error('Icon element not found');
+    iconElement.innerHTML = darkThemeIcon;
 }
 
-document.getElementById('theme-switch').addEventListener('change', (e) => {
-    console.log(e.target.checked);
-    if (e.target.checked) {
+// Get the SVG element. This is the first (and should be the only) child of the container element.
+const svgElement = iconElement.firstChild;
+
+// Set the fill color of the SVG.
+svgElement.style.fill = prefersDarkMode ? 'white' : 'black';
+
+// Add a click event listener to the SVG.
+iconElement.addEventListener('click', () => {
+    if (!document.documentElement.classList.contains('dark-theme')) {
         document.documentElement.classList.add('dark-theme');
+        iconElement.innerHTML = darkThemeIconFilled;
+        const svgElement = iconElement.firstChild; // Update the svgElement reference
+        svgElement.style.fill = 'white'; // Update the fill color to 'white'
     } else {
         document.documentElement.classList.remove('dark-theme');
+        iconElement.innerHTML = darkThemeIcon;
+        const svgElement = iconElement.firstChild; // Update the svgElement reference
+        svgElement.style.fill = 'black';
     }
 });
 
